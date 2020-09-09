05f6a80651819086e35f3585fc680f2a722913b1
Andrew Young <youngar17@gmail.com>
2020-09-09 18:59:00 -0400

    Remove prints from sweep
    Signed-off-by: Andrew Young <youngar17@gmail.com>

--- gc/include/omtalk/Sweep.h
+++ gc/include/omtalk/Sweep.h
@@ -12,19 +12,14 @@ void sweep(GlobalCollectorContext<S> &context, Region &region,
   std::byte *address = region.heapBegin();
   for (const auto object : RegionMarkedObjects<S>(region)) {
     std::byte *objectAddress = reinterpret<std::byte>(object.asRef()).get();
-    std::cout << "!!! region: " << region.heapBegin() << std::endl;
     if (address < objectAddress) {
       std::size_t size = objectAddress - address;
-      std::cout << "!!! add to freelist: " << address << " size: " << size
-                << std::endl;
       freeList.add(address, size);
     }
     address = objectAddress + object.getSize();
   }
   if (address != region.heapEnd()) {
     std::size_t size = region.heapEnd() - address;
-    std::cout << "!!! add region tail to free list " << address
-              << " size: " << size << std::endl;
   }
 }
 
