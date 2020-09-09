01f28f6314ad224e2963c0938399c9cf804a34af
Andrew Young <youngar17@gmail.com>
2020-09-09 19:04:21 -0400

    Add Dispatcher for parallel tasks
    Signed-off-by: Andrew Young <youngar17@gmail.com>

---
+++ gc/test/test-dispatcher.cpp
@@ -0,0 +1,48 @@
+#include <atomic>
+#include <catch2/catch.hpp>
+#include <functional>
+#include <omtalk/Dispatcher.h>
+
+using namespace omtalk::gc;
+
+TEST_CASE("Create worker"
+          "[Worker]") {}
+
+TEST_CASE("Startup 1 threads", "[Dispatcher]") {
+  Dispatcher d(1);
+  REQUIRE(d.getWorkerCount() == 1);
+  d.waitForCompletion();
+  REQUIRE(d.isAllWorkComplete() == true);
+  d.waitForCompletion();
+}
+
+TEST_CASE("Startup 2 threads", "[Dispatcher]") {
+  Dispatcher d(2);
+  REQUIRE(d.getWorkerCount() == 2);
+  d.waitForCompletion();
+  REQUIRE(d.isAllWorkComplete() == true);
+  d.waitForCompletion();
+}
+
+TEST_CASE("1 thread", "[Dispatcher]") {
+  Dispatcher d(1);
+
+  std::atomic<unsigned> i = 0;
+  d.run([&i](WorkerContext &context) {
+    REQUIRE(context.getID() == 0);
+    i.store(1);
+  });
+  d.waitForCompletion();
+  REQUIRE(i.load() == 1);
+}
+
+TEST_CASE("2 threads", "[Dispatcher]") {
+  Dispatcher d(2);
+  std::atomic<unsigned> i[2] = {0};
+  d.run([&i](WorkerContext &context) {
+    i[context.getID()] = context.getID() + 1;
+  });
+  d.waitForCompletion();
+  REQUIRE(i[0] == 1);
+  REQUIRE(i[1] == 2);
+}
