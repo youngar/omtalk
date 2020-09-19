c3300dc811034d49da6ffdfbf3c45e960a25cf53
Andrew Young <youngar17@gmail.com>
2020-09-18 20:08:21 -0400

    Split test-gc into multiple test files
    Signed-off-by: Andrew Young <youngar17@gmail.com>

--- gc/test/test-gc.cpp
+++ gc/test/test-gc.cpp
@@ -7,7 +7,9 @@
 #include <omtalk/Heap.h>
 #include <omtalk/MemoryManager.h>
 #include <omtalk/Ref.h>
-#include <thread>
+
+using namespace omtalk;
+using namespace omtalk::gc;
 
 namespace omtalk::gc {
 template <typename S>
@@ -15,137 +17,30 @@ struct RootWalker;
 } // namespace omtalk::gc
 
 //===----------------------------------------------------------------------===//
-// Test Startup and Shutdown
-//===----------------------------------------------------------------------===//
-
-TEST_CASE("Startup and Shutdown MemoryManager", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
-}
-
-TEST_CASE("Startup and Shutdown MemoryManager and Contexts",
-          "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
-
-  gc::Context<TestCollectorScheme> context1(mm);
-  gc::Context<TestCollectorScheme> context2(mm);
-  gc::Context<TestCollectorScheme> context3(mm);
-}
-
-//===----------------------------------------------------------------------===//
-// Test Exclusive Access
-//===----------------------------------------------------------------------===//
-
-TEST_CASE("Exclusive requested check", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
-
-  gc::Context<TestCollectorScheme> context(mm);
-
-  gc::Context<TestCollectorScheme> context2(mm);
-  std::thread other([&]() { context2.collect(); });
-
-  while (!mm.exclusiveRequested()) {
-  }
-  REQUIRE(context.yieldForGC() == true);
-  other.join();
-}
-
-TEST_CASE("Exclusive Access blocked by other thread", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
-
-  gc::Context<TestCollectorScheme> context(mm);
-  REQUIRE(mm.getContextAccessCount() == 1);
-  REQUIRE(context.yieldForGC() == false);
-  context.collect();
-
-  gc::Context<TestCollectorScheme> context2(mm);
-  std::thread other([&] { context2.collect(); });
-
-  while (!mm.exclusiveRequested()) {
-    // spin
-  }
-
-  while (mm.getContextAccessCount() == 2) {
-    // spin
-  }
-
-  REQUIRE(mm.getContextCount() == 2);
-  REQUIRE(mm.getContextAccessCount() == 1);
-
-  REQUIRE(context.yieldForGC() == true);
-
-  REQUIRE(mm.getContextCount() == 2);
-  REQUIRE(mm.getContextAccessCount() == 2);
-
-  REQUIRE(context.yieldForGC() == false);
-
-  REQUIRE(mm.getContextCount() == 2);
-  REQUIRE(mm.getContextAccessCount() == 2);
-
-  other.join();
-}
-
-TEST_CASE("Exclusive access not blocked by destroyed context",
-          "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
-
-  gc::Context<TestCollectorScheme> context(mm);
-  REQUIRE(mm.getContextAccessCount() == 1);
-  REQUIRE(context.yieldForGC() == false);
-  context.collect();
-
-  {
-    gc::Context<TestCollectorScheme> context2(mm);
-    REQUIRE(mm.getContextAccessCount() == 2);
-    REQUIRE(context.yieldForGC() == false);
-    REQUIRE(context2.yieldForGC() == false);
-  }
-
-  context.collect();
-  REQUIRE(context.yieldForGC() == false);
-}
-
-//===----------------------------------------------------------------------===//
 // Allocation
 //===----------------------------------------------------------------------===//
 
 TEST_CASE("Allocate single garbage", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
 
-  gc::Context<TestCollectorScheme> context(mm);
+  Context<TestCollectorScheme> context(mm);
 
   allocateTestStructObject(context, 10);
 }
 
 TEST_CASE("Allocate single root", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
 
-  gc::Context<TestCollectorScheme> context(mm);
-
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
-
+  Context<TestCollectorScheme> context(mm);
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
   auto ref = allocateTestStructObject(context, 10);
-  gc::Handle<TestStructObject> handle(scope, ref);
+  Handle<TestStructObject> handle(scope, ref);
 }
 
 //===----------------------------------------------------------------------===//
@@ -153,46 +48,43 @@ TEST_CASE("Allocate single root", "[garbage collector]") {
 //===----------------------------------------------------------------------===//
 
 TEST_CASE("Allocate single garbage and gc", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
 
-  gc::Context<TestCollectorScheme> context(mm);
-
+  Context<TestCollectorScheme> context(mm);
   allocateTestStructObject(context, 10);
-
   mm.collect(context);
 }
 
 TEST_CASE("Allocate single root and gc", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
-  gc::Context<TestCollectorScheme> context(mm);
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
+  Context<TestCollectorScheme> context(mm);
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
   auto ref = allocateTestStructObject(context, 10);
-  gc::Handle<TestStructObject> handle(scope, ref);
+  Handle<TestStructObject> handle(scope, ref);
   mm.collect(context);
 }
 
 TEST_CASE("Allocate object tree and gc", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
 
   std::cout << "!!! Start of the good test" << std::endl;
 
-  gc::Context<TestCollectorScheme> context(mm);
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
+  Context<TestCollectorScheme> context(mm);
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
 
   std::cout << "test: handleCount:"
             << mm.getRootWalker().rootScope.handleCount() << std::endl;
 
-  gc::Handle<TestStructObject> handle(scope,
-                                      allocateTestStructObject(context, 10));
+  Handle<TestStructObject> handle(scope, allocateTestStructObject(context, 10));
 
   auto ref = allocateTestStructObject(context, 10);
   ref = allocateTestStructObject(context, 10);
@@ -202,19 +94,19 @@ TEST_CASE("Allocate object tree and gc", "[garbage collector]") {
 }
 
 TEST_CASE("Root scanning", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
 
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
 
-  gc::Context<TestCollectorScheme> context(mm);
+  Context<TestCollectorScheme> context(mm);
 
   for (int i = 0; i < 1; i++) {
 
     auto ref = allocateTestStructObject(context, 10);
-    gc::Handle<TestStructObject> handle(scope, ref);
+    Handle<TestStructObject> handle(scope, ref);
 
     if (ref == nullptr) {
       std::cout << "Bad Allocation\n";
@@ -225,28 +117,28 @@ TEST_CASE("Root scanning", "[garbage collector]") {
 }
 
 TEST_CASE("Check live data", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
   auto &gc = mm.getGlobalCollector();
-  gc::Context<TestCollectorScheme> context(mm);
+  Context<TestCollectorScheme> context(mm);
   auto &gcContext = context.getCollectorContext();
 
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
   unsigned nslots = 10;
   auto allocSize = TestStructObject::allocSize(nslots);
   auto ref = allocateTestStructObject(context, nslots);
-  gc::Handle<TestStructObject> handle(scope, ref);
+  Handle<TestStructObject> handle(scope, ref);
 
   // get the region where the object was allocated
-  auto *region = gc::Region::get(ref);
+  auto *region = Region::get(ref);
 
   SECTION("Marking sets the proper live data size in a region") {
     // perform marking
-    gc.setup(gcContext);
-    gc.scanRoots(gcContext);
-    gc.completeScanning(gcContext);
+    gc.preMark(gcContext);
+    gc.markRoots(gcContext);
+    gc.mark(gcContext);
 
     // Check live data
     REQUIRE(region->getLiveObjectCount() == 1);
@@ -255,37 +147,45 @@ TEST_CASE("Check live data", "[garbage collector]") {
 
   SECTION("Garbage collector resets live data for second GC") {
     // Do it all again! makes sure things are properly reset between GCs
-    gc.setup(gcContext);
-    gc.scanRoots(gcContext);
-    gc.completeScanning(gcContext);
+    gc.preMark(gcContext);
+    gc.markRoots(gcContext);
+    gc.mark(gcContext);
 
     // Check live data
     REQUIRE(region->getLiveObjectCount() == 1);
     REQUIRE(region->getLiveDataSize() == allocSize);
   }
 
-  SECTION("Object is allocated black") {
+  SECTION("Object is allocated white") {
     auto ref = allocateTestStructObject(context, 10);
-    auto *region = gc::Region::get(ref);
+    auto *region = Region::get(ref);
+    REQUIRE(region->unmarked(ref));
+  }
+
+  SECTION("Object is allocated black") {
+    // premark enables black allocation
+    gc.preMark(gcContext);
+    auto ref = allocateTestStructObject(context, 10);
+    auto *region = Region::get(ref);
     REQUIRE(region->marked(ref));
   }
 
   SECTION("Object allocated black during a gc cycle") {
-    gc.setup(gcContext);
+    gc.preMark(gcContext);
     auto ref = allocateTestStructObject(context, 10);
-    auto *region = gc::Region::get(ref);
+    auto *region = Region::get(ref);
     REQUIRE(region->marked(ref));
-    gc.completeScanning(gcContext);
+    gc.mark(gcContext);
     REQUIRE(region->getLiveObjectCount() == 1);
     REQUIRE(region->getLiveDataSize() == allocSize);
   }
 
   SECTION("Object is cleared on the cycle following the current cycle") {
-    gc.setup(gcContext);
+    gc.preMark(gcContext);
     auto ref = allocateTestStructObject(context, 10);
-    auto *region = gc::Region::get(ref);
+    auto *region = Region::get(ref);
     REQUIRE(region->marked(ref));
-    gc.completeScanning(gcContext);
+    gc.mark(gcContext);
     REQUIRE(region->getLiveObjectCount() == 1);
     REQUIRE(region->getLiveDataSize() == allocSize);
     gc.collect(gcContext);
@@ -294,19 +194,19 @@ TEST_CASE("Check live data", "[garbage collector]") {
 }
 
 TEST_CASE("Concurrent", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
   auto &gc = mm.getGlobalCollector();
-  gc::Context<TestCollectorScheme> context(mm);
+  Context<TestCollectorScheme> context(mm);
   auto &gcContext = context.getCollectorContext();
 
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
   unsigned nslots = 10;
   // auto allocSize = TestStructObject::allocSize(nslots);
   auto ref = allocateTestStructObject(context, nslots);
-  gc::Handle<TestStructObject> handle(scope, ref);
+  Handle<TestStructObject> handle(scope, ref);
 
   mm.kickoff(context);
   gc.wait(gcContext);
@@ -317,17 +217,16 @@ TEST_CASE("Concurrent", "[garbage collector]") {
 //===----------------------------------------------------------------------===//
 
 TEST_CASE("Evacuation", "[garbage collector]") {
-  auto mm = gc::MemoryManagerBuilder<TestCollectorScheme>()
-                .withRootWalker(
-                    std::make_unique<gc::RootWalker<TestCollectorScheme>>())
-                .build();
+  auto mm =
+      MemoryManagerBuilder<TestCollectorScheme>()
+          .withRootWalker(std::make_unique<RootWalker<TestCollectorScheme>>())
+          .build();
 
-  gc::Context<TestCollectorScheme> context(mm);
+  Context<TestCollectorScheme> context(mm);
 
-  gc::HandleScope scope = mm.getRootWalker().rootScope.createScope();
+  HandleScope scope = mm.getRootWalker().rootScope.createScope();
 
-  gc::Handle<TestStructObject> handle(scope,
-                                      allocateTestStructObject(context, 10));
+  Handle<TestStructObject> handle(scope, allocateTestStructObject(context, 10));
 
   auto ref = allocateTestStructObject(context, 10);
   ref = allocateTestStructObject(context, 10);
@@ -337,12 +236,12 @@ TEST_CASE("Evacuation", "[garbage collector]") {
   std::cout << "object " << ref << std::endl;
 
   // auto *collector = &mm.getGlobalCollector();
-  // gc::Region *from = gc::Region::get(ref);
-  // gc::Region *to = mm.getRegionManager().allocateRegion();
-  // gc::GlobalCollectorContext<TestCollectorScheme>
+  // Region *from = Region::get(ref);
+  // Region *to = mm.getRegionManager().allocateRegion();
+  // GlobalCollectorContext<TestCollectorScheme>
   // collectorContext(collector);
 
   // collector->scanRoots(collectorContext);
   // collector->completeScanning(collectorContext);
-  // gc::evacuate<TestCollectorScheme>(collectorContext, *from, *to);
+  // evacuate<TestCollectorScheme>(collectorContext, *from, *to);
 }
