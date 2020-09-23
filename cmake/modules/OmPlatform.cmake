f1c18803364bd0b8ca9e3f8468aa235b2b57205e
Devin Nakamura <devin122@gmail.com>
2020-09-23 23:58:55 +0000

    Add missing link flags to sanitizer checks
    Signed-off-by: Devin Nakamura <devin122@gmail.com>

--- cmake/modules/OmPlatform.cmake
+++ cmake/modules/OmPlatform.cmake
@@ -2,6 +2,11 @@ include (CheckCCompilerFlag)
 
 # Note we are assuming that if the c compiler accepts the flag, the cxx compiler will as well
 
+set(CMAKE_REQUIRED_LINK_OPTIONS -fsanitize=thread)
 check_c_compiler_flag(-fsanitize=thread OM_HAVE_TSAN)
+
+set(CMAKE_REQUIRED_LINK_OPTIONS -fsanitize=address)
 check_c_compiler_flag(-fsanitize=address OM_HAVE_ASAN)
+
+set(CMAKE_REQUIRED_LINK_OPTIONS -fsanitize=undefined)
 check_c_compiler_flag(-fsanitize=undefined OM_HAVE_UBSAN)
