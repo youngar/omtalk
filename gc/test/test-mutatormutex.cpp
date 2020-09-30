c9424430f0b9c9dd1dc4b0c3b16d003e4854b28c
Andrew Young <youngar17@gmail.com>
2020-09-30 11:08:26 -0400

    Add MutatorMutex to pause mutators
    MutatorMutex is intended to keep track of mutators which are not
    currently at safe points, and provide the GC with the capability to
    signal an exclusive request.
    
    There is no shared_lock provided for attaching and detaching access
    through the mutator.
    
    Signed-off-by: Andrew Young <youngar17@gmail.com>

---
+++ gc/test/test-mutatormutex.cpp
@@ -0,0 +1,43 @@
+#include <catch2/catch.hpp>
+#include <omtalk/MutatorMutex.h>
+#include <thread>
+
+using namespace omtalk::gc;
+
+TEST_CASE("MutatorMutex no request", "[garbage collector]") {
+
+  MutatorMutex m;
+  REQUIRE(m.requested() == false);
+  m.attach();
+  REQUIRE(m.requested() == false);
+  m.detach();
+  REQUIRE(m.requested() == false);
+}
+
+TEST_CASE("MutatorMutex request", "[garbage collector]") {
+  MutatorMutex m;
+  m.lock();
+  std::thread thread([&m] {
+    m.attach();
+    m.detach();
+  });
+  m.unlock();
+  thread.join();
+}
+
+TEST_CASE("MutatorLock requested", "[garbage collector]") {
+  MutatorMutex m;
+  MutatorLock l(m);
+  REQUIRE(m.requested());
+}
+
+TEST_CASE("MutatorLock request", "[garbage collector]") {
+  MutatorMutex m;
+  MutatorLock l(m);
+  std::thread thread([&m] {
+    m.attach();
+    m.detach();
+  });
+  l.unlock();
+  thread.join();
+}
