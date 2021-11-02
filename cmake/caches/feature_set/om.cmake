5d13f31a42e5018d4933ab292ce635b8e9edb6fd
Robert Young <robert.young@shopify.com>
2021-11-01 21:28:04 -0400

    Disable compiler support for om by default

--- cmake/caches/feature_set/om.cmake
+++ cmake/caches/feature_set/om.cmake
@@ -1,5 +1,8 @@
 set(OM_COMP_AB ON CACHE BOOL "")
 set(OM_COMP_OM ON CACHE BOOL "")
-set(OM_COMP_OM_COMPILER ON CACHE BOOL "")
+
 set(OM_COMP_OM_GC ON CACHE BOOL "")
 set(OM_COMP_OM_OM ON CACHE BOOL "")
+
+# Compiler support is disabled by default
+set(OM_COMP_OM_COMPILER OFF CACHE BOOL "")
